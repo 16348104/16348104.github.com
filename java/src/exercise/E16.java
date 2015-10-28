3e47695222ae5d63c022decc01f60022ce1672e4
dell <16348104@qq.com>
2015-10-29 01:07:08 +0800

    update javaSE 10-29/27

--- java/src/exercise/E16.java
+++ java/src/exercise/E16.java
@@ -5,4 +5,14 @@ package exercise;
  * 输出9*9口诀。
  */
 public class E16 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+
+            for (int j = 1; j < 10; j++) {
+                System.out.print(+i + "*" + j + "=" + i * j+"\t" );
+            }
+            System.out.println();
+        }
+    }
 }
+
