1f7b26ec16d48d405166cc3f55d7581a825cfa65
dell <16348104@qq.com>
2015-10-29 00:19:44 +0800

    update javaSE 10-29/27

--- java/src/exercise/E27.java
+++ java/src/exercise/E27.java
@@ -5,4 +5,19 @@ package exercise;
  * 求100之内的素数。
  */
 public class E27 {
+    public static void main(String[] args) {
+        for (int i = 2; i < 101; i++) {
+            boolean b = true;
+            for (int j = 2; j < i / 2 + 1; j++) {
+                if (i % j == 0) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
 }
