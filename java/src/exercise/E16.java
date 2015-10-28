3d39a7a6eec833c2fdec81f68c5133bff7548460
dell <16348104@qq.com>
2015-10-29 01:11:09 +0800

    update javaSE 10-29/27

--- java/src/exercise/E16.java
+++ java/src/exercise/E16.java
@@ -8,7 +8,7 @@ public class E16 {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
 
-            for (int j = 1; j < 10; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(+i + "*" + j + "=" + i * j+"\t" );
             }
             System.out.println();
