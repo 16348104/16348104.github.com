743cc698031ac65a1d759e7500982d9fe2d84796
dell <16348104@qq.com>
2015-10-29 21:44:58 +0800

    update javaSE 10-29

---
+++ java/src/inherited/Father.java
@@ -0,0 +1,41 @@
+package inherited;
+
+/**
+ * Created by dell on 2015/10/29.
+
+
+ */
+public class Father {
+
+    int i;
+
+    void test() {
+       System.out.println("test method in father class...");
+        i ++;
+    }
+}
+
+class Son extends Father {
+
+//   void test() {
+//      System.out.println("test method in son class...");
+//   }
+
+    Son() {
+        super();
+        System.out.println("in son constructor...");
+    }
+
+       @Override// 注解
+    void test() {
+        int j = i;
+        super.test();
+        System.out.println("test method in son class...");
+    }
+
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.test();
+    }
+}
+
