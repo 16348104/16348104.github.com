a8bcc5fec09dc501df35160b82a82bc48f3f1173
16348104 <16348104@QQ.com>
2015-11-05 21:44:09 +0800

    Update  pass

--- java/src/inherited/PassTest.java
+++ java/src/inherited/PassTest.java
@@ -7,33 +7,33 @@ package inherited;
 
         private int i;// Integer
 
-        public static void method(PassTest passTest) {// 形式参数
+        public static void method(PassTest passTest) {// 形式参数 类类型
             System.out.println("b: i = " + passTest.i);
             passTest.i += 1000;
             System.out.println("c: i = " + passTest.i);
         }
 
-        public static void test(String string) {
-            string += "test";
-        }
+//        public static void test(String string) {
+//            string += "test";
+//        }
 
-        public static void integerTest(Integer i) {
-            i++;
-        }
+//        public static void integerTest(Integer i) {
+//            i++;
+//        }
 
         public static void main(String[] args) {
             PassTest passTest = new PassTest();
 //        passTest.i = 0;
             System.out.println("a: i = " + passTest.i);
             method(passTest);// 实际参数
-            System.out.println("d: i = " + passTest.i);// ?
+            System.out.println("d: i = " + passTest.i);
 
             String string = "测试";
-            test(string);
-            System.out.println(string);
+//            test(string);
+//            System.out.println(string);
 
             int i = 0;
-            integerTest(i);
+//            integerTest(i);
             System.out.println(i);
         }
     }
