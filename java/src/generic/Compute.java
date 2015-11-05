3c42b8f62bc3a506f6072c153a576c5dbe626421
16348104 <16348104@QQ.com>
2015-11-05 22:29:13 +0800

    Update  generic

--- java/src/generic/Compute.java
+++ java/src/generic/Compute.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 /**
  * Created by Administrator on 2015/11/1.
  */
-public class Compute<T extends Object& Serializable & Comparable,S>{
-    public String add(T x,S y) {
+public class Compute<T extends Object & Serializable & Comparable, S> {
+    public String add(T x, S y) {
         return String.valueOf(x) + String.valueOf(y);
     }
+
     public static void main(String[] args) {
-        Compute <Double,Boolean>co= new Compute<>();
+        Compute<Double, Boolean> co = new Compute<>();
 //        Compute <Integer>co= new Compute<>();
 
         System.out.println(co.add(2.5, false));
