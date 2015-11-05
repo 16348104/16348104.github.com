9ad74a3597a17a86bbce5a69ad78a1d71cadc086
16348104 <16348104@QQ.com>
2015-11-05 22:08:46 +0800

    Update  static

---
+++ java/src/StaticTest.java
@@ -0,0 +1,26 @@
+/**
+ * Created by xdx on 2015/11/5.
+ */
+public class StaticTest {
+    private String name;
+    private int age;
+
+    public static void m1() {
+
+    }
+
+    public static void m2() {
+
+              m1();
+    }
+
+    public StaticTest(String name, int age) {//alt+insert
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
