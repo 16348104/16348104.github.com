f47080cd4586ce74f6d8b324c232c3792b349863
16348104 <16348104@QQ.com>
2015-11-05 22:44:26 +0800

    Update  generic

--- java/src/generic/GenericInterface.java
+++ java/src/generic/GenericInterface.java
@@ -3,17 +3,18 @@ package generic;
 /**
  * Created by xdx on 2015/11/5.
  */
-public interface GenericInterface <T>{
+public interface GenericInterface<T> {
     void m1(T m);
 }
-    class Test<T> implements GenericInterface<T> {
-        @Override
-        public void m1(T m) {
 
-        }
+class Test<T> implements GenericInterface<T> {
+    @Override
+    public void m1(T m) {
 
-        public static void main(String[] args) {
-            Test<Double> test;
-            test = new Test<>();
-        }
     }
+
+    public static void main(String[] args) {
+        Test<Double> test;
+        test = new Test<>();
+    }
+}
