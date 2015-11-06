0d47da20fc77590f9cc682026df54ac8633442a3
16348104 <16348104@QQ.com>
2015-11-06 22:58:52 +0800

    Update  vector

---
+++ java/src/VectorTest.java
@@ -0,0 +1,25 @@
+import java.util.Vector;
+
+/**
+ * Created by xdx on 2015/11/6.
+ */
+public class VectorTest {
+
+    public static void main(String[] args) {
+
+        Vector<String> vector = new Vector<>();
+        vector.add("a");
+        vector.add("s");
+        vector.add("d");
+        vector.add("f");
+        vector.add("dghghghghghghg");
+        System.out.println(vector.size());
+        System.out.println(vector.get(4));
+        vector.clear();
+        System.out.println(vector.size());
+        Vector<Integer> vector1 = new Vector<>();
+        vector1.add(100);
+        System.out.println(vector1.get(0));
+
+    }
+}
