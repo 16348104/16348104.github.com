525474b328e42277b77c7d4e7896fa44e48c20bc
16348104 <16348104@QQ.com>
2015-11-06 22:06:51 +0800

    Update  exercise

--- java/src/exercise/E12.java
+++ java/src/exercise/E12.java
@@ -1,5 +1,7 @@
 package exercise;
 
+import java.util.Scanner;
+
 /**
  * Created by dell on 2015/10/28.
  * 企业发放的奖金根据利润提成。利润(I)低于或等于10万元时，奖金可提10%；利润高于10万元，低于20万元时，低于10万元的部分按10%提成，
@@ -8,4 +10,32 @@ package exercise;
  * 求应发放奖金总数？
  */
 public class E12 {
+
+    public static void main(String[] args) {
+
+        System.out.println("请输入利润：");
+
+        Scanner s = new Scanner(System.in);
+        double i = s.nextDouble();
+        double sum = 0;
+        if (i <= 100000) {
+            sum = i * 0.1;
+        }
+        if ((i > 100000) && (i < 200000)) {
+            sum += (i - 10000) * 0.075;
+        }
+        if ((i >= 200000) && (i < 400000)) {
+            sum += sum + (i - 200000) * 0.05;
+        }
+        if ((i >= 400000) && (i < 600000)) {
+            sum += (i - 400000) * 0.03;
+        }
+        if ((i >= 600000) && (i < 1000000)) {
+            sum += (i - 60000) * 0.015;
+        }
+        if (i > 100000) {
+            sum += (i - 100000) * 0.01;
+        }
+        System.out.print("应发放奖金总额："+sum);
+    }
 }
