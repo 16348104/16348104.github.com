6bf9416e1ee66d352e2f04656f971c272bf33343
16348104 <16348104@QQ.com>
2015-11-19 15:48:04 +0800

    update E22938

---
+++ java/src/exercise/E29.java
@@ -0,0 +1,8 @@
+package exercise;
+
+/**
+ * Created by xdx on 2015/11/19.
+ * 求一个3*3矩阵对角线元素之和。
+ */
+public class E29 {
+}
---
+++ java/src/exercise/E38.java
@@ -0,0 +1,9 @@
+package exercise;
+
+/**
+ * Created by xdx on 2015/11/19.
+ * 写一个函数，求一个字符串的长度，在main函数中输入字符串，并输出其长度。
+ */
+public class E38
+{
+}
