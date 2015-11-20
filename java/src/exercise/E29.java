9b2305e318fc2514b914b35b371876f075cae554
16348104 <16348104@QQ.com>
2015-11-21 04:46:45 +0800

    update E29

--- java/src/exercise/E29.java
+++ java/src/exercise/E29.java
@@ -1,8 +1,32 @@
 package exercise;
 
+import sun.text.resources.cldr.ia.FormatData_ia;
+
+import java.util.Scanner;
+
 /**
  * Created by xdx on 2015/11/19.
  * 求一个3*3矩阵对角线元素之和。
  */
 public class E29 {
+    public static void main(String[] args) {
+        System.out.println("请输入整数：");
+    Scanner scanner = new Scanner(System.in);
+    int input[][]=new int[][];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+            }
+
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j <a.length ; j++) {
+                System.out.print(a[i][j]);
+
+            }
+            System.out.println();
+
+        }
+    }
+
 }
--- java/src/exercise/E38.java
+++ java/src/exercise/E38.java
@@ -1,22 +1,9 @@
 package exercise;
 
-import java.util.Scanner;
-
 /**
  * Created by xdx on 2015/11/19.
  * 写一个函数，求一个字符串的长度，在main函数中输入字符串，并输出其长度。
  */
-public class E38 {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入字符串：");
-        String string = scanner.next();
-        System.out.println("字符串长度："+mystr(string));
-    }
-
-   static int mystr(String s) {
-      return s.length();
-   }
-
+public class E38
+{
 }
