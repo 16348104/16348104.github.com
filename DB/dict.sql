cf8c56ff8a536dda401b365c9bf64ccb84c785e4
16348104 <16348104@QQ.com>
2015-11-22 23:18:47 +0800

    update E29

--- DB/dict.sql
+++ DB/dict.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS demo4.word;
 CREATE TABLE demo4.word (
   word_id    SMALLINT(8) UNIQUE AUTO_INCREMENT PRIMARY KEY
   COMMENT '词汇编号',
-  vocabulary TEXT NOT NULL
+  vocabulary TEXT(255) NOT NULL
   COMMENT '词汇',
   ipa_uk     VARCHAR(255) COMMENT '英语音标',
   ipa_us     VARCHAR(255) COMMENT '美语音标'
@@ -22,14 +22,10 @@ CREATE TABLE demo4.character (
   character_id SMALLINT(8) UNIQUE
   COMMENT '词性标号',
   CONSTRAINT fk_ch_word_id FOREIGN KEY (character_id) REFERENCES demo4.word (word_id),
-  explain      TEXT COMMENT '解释'
+  explain      TEXT COMMENT '词汇解释'
 
-)
-  COMMENT '词性表';
-
-SHOW FULL COLUMNS FROM demo4.character;
-
-
+  COMMENT '词性表'
+);
 DROP TABLE IF EXISTS demo4.example;
 CREATE TABLE demo4.example (
   id         INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
@@ -37,20 +33,9 @@ CREATE TABLE demo4.example (
   example_id SMALLINT(8) UNIQUE
   COMMENT '例句号',
   CONSTRAINT fk_ex_id FOREIGN KEY (example_id) REFERENCES demo4.character (id),
-  Chinese    LONGTEXT COMMENT '中文例句',
-  English    LONGTEXT COMMENT '英语例句',
-  web_origin VARCHAR(255) COMMENT '网站来源'
+  Chinese    TEXT(255) COMMENT '中文例句',
+  English    TEXT(255) COMMENT '英语例句',
+  web        VARCHAR(255) COMMENT '网站'
 
-
+  COMMENT '例句表'
 )
-  COMMENT '例句表';
-SHOW FULL COLUMNS FROM demo4.example;
-
-# alter TABLE MODIFY
-CREATE TABLE demo4.test
-(
-   i TEXT COMMENT 'test'
-);
-INSERT INTO demo4.test VALUES ('ghghg');
-SELECT *
-FROM demo4.test;
--- java/src/exercise/E29.java
+++ java/src/exercise/E29.java
@@ -12,16 +12,21 @@ public class E29 {
         int s = 0;
         System.out.println("请输入整数：");
         Scanner scanner = new Scanner(System.in);
-        int a[][] = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        int a[][] = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] a = new int[3][3];
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j]);
-                if (i == j) {
-                    s += a[i][j];
-                }
+                a[i][j] = scanner.nextInt();
+//
             }
-            System.out.println();
-
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (i == j) {
+                    s += a[i][j];}
+                System.out.print(a[i][j]);
+            }
+            System.out.println("");
         }
         System.out.println("3*3矩阵对角线元素之和:" + s);
     }
