75339ce9d99027f27638435c9db33cf1d13444a9
16348104 <16348104@QQ.com>
2015-11-23 21:50:03 +0800

    update E38

--- java/src/exercise/E38.java
+++ java/src/exercise/E38.java
@@ -1,9 +1,20 @@
 package exercise;
 
+import java.util.Scanner;
+
 /**
  * Created by xdx on 2015/11/19.
  * 写一个函数，求一个字符串的长度，在main函数中输入字符串，并输出其长度。
  */
-public class E38
-{
+public class E38 {
+    public static int m(String s) {
+        return s.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("请输入一串字符：");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(m(str));
+    }
 }
