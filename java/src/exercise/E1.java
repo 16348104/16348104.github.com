157abbca678cee4b01de370e1a4a5a0f2d97bffc
16348104 <16348104@QQ.com>
2015-12-01 20:48:22 +0800

    update E1

---
+++ java/src/exercise/E1.java
@@ -0,0 +1,14 @@
+package exercise;
+
+/**
+ * Created by xdx on 2015/12/1.
+ * 古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，
+ * 小兔子长到第三个月后每个月又生一对兔子，
+ * 假如兔子都不死，问每个月的兔子总数为多少？
+ */
+public class E1 {
+    public static void main(String[] args) {
+        int i;
+        System.out.println();
+    }
+}
