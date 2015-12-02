e7561c5916d26448e803b2f1affe003240f56ad5
16348104 <16348104@QQ.com>
2015-12-02 23:18:00 +0800

    update javaweb5

--- HelloServlet/src/com/yxtech/FirstServlet.java
+++ HelloServlet/src/com/yxtech/FirstServlet.java
@@ -1,7 +1,57 @@
 package com.yxtech;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by xdx on 2015/12/2.
  */
-public class FirstServlet {
+@WebServlet(
+        value = "/FirstServlet"
+)
+
+public class FirstServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("初始化init()");
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("调用doget()方法");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head><title>测试0-10的循环结果</title>");
+        out.println("<body>");
+        out.println("开始执行......");
+        int count = 0;
+        for (int i = 0; i <= 10; i++) {
+            count += i;
+        }
+        out.println("程序执行结果:" + count);
+        out.println("</body>");
+        out.println("</html>");
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("调用destroy()方法");
+    }
 }
---
+++ HelloServlet/web/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xdx
+  Date: 2015/12/2
+  Time: 21:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+    <form action =FirstServlet  method="get">
+
+    <input type="submit" value="firstservlet">
+
+
+  </form>
+
+  <h1>END</h1>
+  </body>
+</html>
