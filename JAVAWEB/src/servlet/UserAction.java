e90bc4a29a7ae7affcb803710ee172eecc67ef00
16348104 <16348104@QQ.com>
2015-12-12 02:30:46 +0800

    javaee

--- JAVAWEB/src/servlet/UserAction.java
+++ JAVAWEB/src/servlet/UserAction.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 @WebServlet("/user")
 public class UserAction extends HttpServlet{
     @Override
